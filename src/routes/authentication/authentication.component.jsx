3560ac20f7e0c926cdc41fe68f4e72c9839b1126
Will Alread <will.alread@gmail.com>
2023-03-03 07:00:01 -0500

    lesson-9

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,10 +1,11 @@
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
 
+import './authentication.styles.scss';
+
 function Authentication() {
   return (
-    <div>
-      <h1>Sign In Page</h1>
+    <div className='authentication-container'>
       <SignInForm />
       <SignUpForm />
     </div>
