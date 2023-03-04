6b79e0030e0f4251c4fa68d66bdc4f448cea4a2b
Will Alread <will.alread@gmail.com>
2023-03-04 09:04:49 -0500

    fixed google sign in button

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -7,7 +7,10 @@ const BUTTON_TYPE_CLASSES = {
 
 function Button({ children, buttonType, ...otherProps }) {
   return (
-    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}>
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
       {children}
     </button>
   );
