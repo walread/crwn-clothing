d424cc97b95b19a83f85f85b216c2efbbe932d23
Will Alread <will.alread@gmail.com>
2023-03-04 07:34:43 -0500

    lesson-10

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -71,3 +72,5 @@ export async function signInAuthUserWithEmailAndPassword(email, password) {
 
   return await signInWithEmailAndPassword(auth, email, password);
 }
+
+export const signOutUser = async () => await signOut(auth);
