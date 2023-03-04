7f992d0406d33519ac9e36d02884ee47dc20ea65
Will Alread <will.alread@gmail.com>
2023-03-04 08:20:29 -0500

    lesson-11

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -74,3 +75,6 @@ export async function signInAuthUserWithEmailAndPassword(email, password) {
 }
 
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
